38f5f50cb7353e7b48625bac6a97a7d115a97451
root <root@localhost.localdomain>
2016-09-12 18:14:35 -0300

    primeiro test unitario de status code

--- app/provider_test.py
+++ app/provider_test.py
@@ -1,12 +1,15 @@
-# # testing api rest provider model
-# import os
-# import flaskr
-# import unittest
-#
-# from provider import Provider
-#
-# class ProviderTestCase(unittest.TestCase):
-#     # 0
-#     # 0
-#     def test_is_request_param(self):
-#         pass
+# testing api rest provider model
+import os
+import unittest
+
+class ProviderTestCase(unittest.TestCase):
+    # 0
+    def test_success(self):
+        from provider import app
+        with app.test_client() as client:
+            response = client.get('/apis/providers')
+            self.assertEqual(200,response.status_code)
+
+
+if __name__ == '__main__':
+    unittest.main()
